06f189f1f359f4cc9c2615080e702d8fecb0d033
Mohsina-Ahmed <122023111+Mohsina-Ahmed@users.noreply.github.com>
2024-02-23 10:23:14 +0000

    Updating databases with 2 splits for saving authentications and activities

--- activity-tracking/server.js
+++ activity-tracking/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./config.json');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config.json");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5300;
@@ -15,25 +15,27 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(mongoUri, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB database connection established successfully"))
+  .connect(mongoUri, { useNewUrlParser: true, dbName: "activitydb" })
+  .then(() =>
+    console.log("MongoDB database connection established successfully")
+  )
   .catch((error) => console.error("MongoDB connection error:", error));
 
 const connection = mongoose.connection;
 
 // Event listener for MongoDB connection errors
-connection.on('error', (error) => {
+connection.on("error", (error) => {
   console.error("MongoDB connection error:", error);
 });
 
 // Routes
-const exercisesRouter = require('./routes/exercises');
-app.use('/exercises', exercisesRouter);
+const exercisesRouter = require("./routes/exercises");
+app.use("/exercises", exercisesRouter);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 // Start the server
@@ -41,4 +43,4 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-module.exports = app;  
+module.exports = app;
