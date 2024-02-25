942239f66983f3737b15472d6b52b5a1c6bfec18
Carlie Ede <carlieede@gmail.com>
2024-02-25 10:16:33 +0000

    connected to activitydb

--- analytics/test_app.py
+++ analytics/test_app.py
@@ -1,6 +1,5 @@
 # the magic command!
-# docker compose up --build -d analytics
-# docker compose run analytics pytest . --setup-show -rA 
+# docker compose run --build --rm analytics pytest -rA --setup-show 
 # -rA = get test summary for all (i.e. see stdout print statements) 
 # https://doc.pytest.org/en/latest/reference/reference.html#command-line-flags
 
@@ -10,14 +9,13 @@ from flask.testing import FlaskClient
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
 import os
-from app import create_app, db
+from app import app
 
 @pytest.fixture(scope='session')
 def test_client():
-    app, db = create_app()
+    # app, db = create_app(test=True)
     app.testing = True
-    # app.config['TESTING'] = True
-    app.config['MONGO_URI'] = os.getenv('MONGO_URI') + '/test_database'
+    app.config['MONGO_DB'] = 'test_database'
     # Create a test client using the Flask application configured for testing
     with app.test_client() as testing_client: 
         # # Establish an application context
@@ -26,43 +24,64 @@ def test_client():
     
 
 @pytest.fixture(scope='session')
-def mongo_client(test_client):
+def mongo_client():
     
-    mongo = PyMongo()
-    mongo.init_app(test_client.application)
-    print(mongo.db)
-    # client = MongoClient(os.getenv('MONGO_URI') + '/?timeoutMS=1000')  # Use a test database  
-    
+    # mongo = PyMongo()
+    # print(test_client.application.config['MONGO_URI'])
+    # mongo.init_app(test_client.application, connect=True)
+    # print(mongo.db)
+    client = MongoClient(os.getenv('MONGO_URI') + '/?timeoutMS=1000')  # Use a test database  
+    print(client.admin.command('ping'))
+    db = client['test_database']
+    print(db)
     # Insert some test data into the MongoDB test database before testing
-    mongo.db.exercises.insert_one({
+    # print(mongo.db.test_database)
+    # db = mongo.cx.get_database('test_database')
+    # collection = db['exercises']
+    db.exercises.insert_many(
+    [{
         "username": "test_user",
         "exerciseType": "running",
         "duration": 30,
         "date": "2022-01-01"
-    })
+    }, 
+    {
+        "username": "test_user",
+        "exerciseType": "running",
+        "duration": 30,
+        "date": "2022-01-02"
+    }, 
+    {
+        "username": "test_user",
+        "exerciseType": "cycling",
+        "duration": 30,
+        "date": "2022-01-03"
+    }])
 
-    yield mongo
+    yield client
 
     # Clean up the test database after tests
-    mongo.cx.drop_database('test_database')  
+    # client.drop_database('test_database')  
     # client.close()
 
 # check the connection to the mongo client 
 def test_mongo_connection(mongo_client):
-    assert mongo_client.cx.admin.command("ping")["ok"] == 1.0
+    assert mongo_client.admin.command("ping")["ok"] == 1.0
 
 # check the index route of the Flask App - get all exercises from mongo 
 # for now just want to check there is a response... 
 # TODO: test route and data from test_database
 def test_index_route(test_client):
     response = test_client.get('/')
+    print(response.data)
     assert response.status_code == 200
+
 
 def test_stats_route(test_client, mongo_client):
     response = test_client.get('/stats')
     assert response.status_code == 200
-    assert b'test_user' in response.data
-    assert b'running' in response.data
+#     assert b'test_user' in response.data
+#     assert b'running' in response.data
 
 # def test_user_stats_route(client, mongo_client):
 #     # Insert some more test data into the MongoDB test database before testing
