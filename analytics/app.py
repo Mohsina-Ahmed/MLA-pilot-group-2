ec441f82911181dffef2e40503cdbac24983c573
Carlie Ede <carlieede@gmail.com>
2024-02-25 12:04:20 +0000

    functional test cases!

--- analytics/app.py
+++ analytics/app.py
@@ -18,164 +18,159 @@ def create_app(test=False):
     load_dotenv()
     mongo_uri = os.getenv('MONGO_URI')
     app.config['MONGO_URI'] = mongo_uri
-    # configure database for pytest if needed - TODO: resolve
+    # configure using app config - TODO: resolve
     if test:
         mongo_db = 'test_database'
-        print('i am here')
     else: 
         mongo_db = os.getenv('MONGO_DB') 
 
     # connect to mongo client
-    mongo = MongoClient(mongo_uri)
-    db = mongo[mongo_db]
+    # mongo = MongoClient(mongo_uri)
+    # db = mongo[mongo_db]
 
-    # mongo = PyMongo()
-    # mongo.init_app(app, connect=True)
+    mongo = PyMongo()
+    mongo.init_app(app, connect=True)
     
     # print(app.config['MONGO_URI'])
     print(mongo_db)
-    # db = mongo.cx.get_database(mongo_db)
-    print(db)
+    db = mongo.cx.get_database(mongo_db)
 
-    return app, db
-
-app, db = create_app()
-
-@app.route('/')
-def index():
-    exercises = db.exercises.find()
-    exercises_list = list(exercises)
-    return json_util.dumps(exercises_list)
+    @app.route('/')
+    def index():
+        exercises = db.exercises.find()
+        exercises_list = list(exercises)
+        return json_util.dumps(exercises_list)
 
 
-@app.route('/stats')
-def stats():
-    pipeline = [
-        {
-            "$group": {
-                "_id": {
-                    "username": "$username",
-                    "exerciseType": "$exerciseType"
-                },
-                "totalDuration": {"$sum": "$duration"}
-            }
-        },
-        {
-            "$group": {
-                "_id": "$_id.username",
-                "exercises": {
-                    "$push": {
-                        "exerciseType": "$_id.exerciseType",
-                        "totalDuration": "$totalDuration"
+    @app.route('/stats')
+    def stats():
+        pipeline = [
+            {
+                "$group": {
+                    "_id": {
+                        "username": "$username",
+                        "exerciseType": "$exerciseType"
+                    },
+                    "totalDuration": {"$sum": "$duration"}
+                }
+            },
+            {
+                "$group": {
+                    "_id": "$_id.username",
+                    "exercises": {
+                        "$push": {
+                            "exerciseType": "$_id.exerciseType",
+                            "totalDuration": "$totalDuration"
+                        }
                     }
                 }
-            }
-        },
-        {
-            "$project": {
-                "username": "$_id",
-                "exercises": 1,
-                "_id": 0
-            }
-        }
-    ]
-
-    stats = list(db.exercises.aggregate(pipeline))
-    return jsonify(stats=stats)
-
-
-@app.route('/stats/<username>', methods=['GET'])
-def user_stats(username):
-    pipeline = [
-        {
-            "$match": {"username": username}
-        },
-        {
-            "$group": {
-                "_id": {
-                    "username": "$username",
-                    "exerciseType": "$exerciseType"
-                },
-                "totalDuration": {"$sum": "$duration"}
-            }
-        },
-        {
-            "$group": {
-                "_id": "$_id.username",
-                "exercises": {
-                    "$push": {
-                        "exerciseType": "$_id.exerciseType",
-                        "totalDuration": "$totalDuration"
-                    }
+            },
+            {
+                "$project": {
+                    "username": "$_id",
+                    "exercises": 1,
+                    "_id": 0
                 }
             }
-        },
-        {
-            "$project": {
-                "username": "$_id",
-                "exercises": 1,
-                "_id": 0
-            }
-        }
-    ]
+        ]
 
-    stats = list(db.exercises.aggregate(pipeline))
-    return jsonify(stats=stats)
-
-
-@app.route('/stats/weekly/', methods=['GET'])
-def weekly_user_stats():
-    username = request.args.get('user')
-    start_date_str = request.args.get('start')
-    end_date_str = request.args.get('end')
-
-    date_format = "%d-%m-%Y"
-    try:
-        start_date = datetime.strptime(start_date_str, date_format)
-        end_date = datetime.strptime(end_date_str, date_format) + timedelta(days=1)  # Include the whole end day
-
-        logging.info(f"Fetching weekly stats for user: {username} from {start_date} to {end_date}")
-    except Exception as e:
-        logging.error(f"Error parsing dates: {e}")
-        return jsonify(error="Invalid date format"), 400
-
-    pipeline = [
-        {
-            "$match": {
-                "username": username,
-                "date": {
-                    "$gte": start_date,
-                    "$lt": end_date
-                }
-            }
-        },
-        {
-            "$group": {
-                "_id": {
-                    "exerciseType": "$exerciseType"
-                },
-                "totalDuration": {"$sum": "$duration"}
-            }
-        },
-        {
-            "$project": {
-                "exerciseType": "$_id.exerciseType",
-                "totalDuration": 1,
-                "_id": 0
-            }
-        }
-    ]
-
-    try:
         stats = list(db.exercises.aggregate(pipeline))
         return jsonify(stats=stats)
-    except Exception as e:
-        current_app.logger.error(f"An error occurred while querying MongoDB: {e}")
-        traceback.print_exc()
-        return jsonify(error="An internal error occurred"), 500
 
+
+    @app.route('/stats/<username>', methods=['GET'])
+    def user_stats(username):
+        pipeline = [
+            {
+                "$match": {"username": username}
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "username": "$username",
+                        "exerciseType": "$exerciseType"
+                    },
+                    "totalDuration": {"$sum": "$duration"}
+                }
+            },
+            {
+                "$group": {
+                    "_id": "$_id.username",
+                    "exercises": {
+                        "$push": {
+                            "exerciseType": "$_id.exerciseType",
+                            "totalDuration": "$totalDuration"
+                        }
+                    }
+                }
+            },
+            {
+                "$project": {
+                    "username": "$_id",
+                    "exercises": 1,
+                    "_id": 0
+                }
+            }
+        ]
+
+        stats = list(db.exercises.aggregate(pipeline))
+        return jsonify(stats=stats)
+
+
+    @app.route('/stats/weekly/', methods=['GET'])
+    def weekly_user_stats():
+        username = request.args.get('user')
+        start_date_str = request.args.get('start')
+        end_date_str = request.args.get('end')
+
+        date_format = "%d-%m-%Y"
+        try:
+            start_date = datetime.strptime(start_date_str, date_format)
+            end_date = datetime.strptime(end_date_str, date_format) + timedelta(days=1)  # Include the whole end day
+
+            logging.info(f"Fetching weekly stats for user: {username} from {start_date} to {end_date}")
+        except Exception as e:
+            logging.error(f"Error parsing dates: {e}")
+            return jsonify(error="Invalid date format"), 400
+
+        pipeline = [
+            {
+                "$match": {
+                    "username": username,
+                    "date": {
+                        "$gte": start_date,
+                        "$lt": end_date
+                    }
+                }
+            },
+            {
+                "$group": {
+                    "_id": {
+                        "exerciseType": "$exerciseType"
+                    },
+                    "totalDuration": {"$sum": "$duration"}
+                }
+            },
+            {
+                "$project": {
+                    "exerciseType": "$_id.exerciseType",
+                    "totalDuration": 1,
+                    "_id": 0
+                }
+            }
+        ]
+
+        try:
+            stats = list(db.exercises.aggregate(pipeline))
+            return jsonify(stats=stats)
+        except Exception as e:
+            current_app.logger.error(f"An error occurred while querying MongoDB: {e}")
+            traceback.print_exc()
+            return jsonify(error="An internal error occurred"), 500
+
+    return app
 
 
 if __name__ == "__main__":
-    # app, db = create_app()
+    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5050)
