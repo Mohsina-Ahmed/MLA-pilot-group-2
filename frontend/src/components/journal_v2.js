4ee47ec88ce3453a85151ae224fb4f56ebf20899
Carlie Ede <carlieede@gmail.com>
2024-02-29 07:53:24 +0000

    implement front end list render as function - handles case with no exercises returned

--- frontend/src/components/journal_v2.js
+++ frontend/src/components/journal_v2.js
@@ -34,47 +34,19 @@ const Journal = ({ currentUser }) => {
   const [endDate, setEndDate] = useState(moment().endOf('week').toDate());
   const [exercises, setExercises] = useState([]);
 
-  // const fetchExercises = async () => {
-  //   try {
-  //     const url = `http://localhost:5050/stats/weekly/?user=${currentUser}&start=${moment(startDate).format('DD-MM-YYYY')}&end=${moment(endDate).format('DD-MM-YYYY')}`;
-  //     const response = await axios.get(url);
-  //     console.log('API Response:', response.data);
-  //     if (response.data.stats && Array.isArray(response.data.stats)) {
-  //       setExercises(response.data.stats);
-  //     } else {
-  //       console.error('Unexpected response structure:', response.data);
-  //       setExercises([]);
-  //     }
-  //   } catch (error) {
-  //     console.error('Failed to fetch exercises', error);
-  //   }
-  // };
-
-  console.log('I am here')
   const fetchExercises = async () => {
     try {
-      // const url = `http://localhost:5050/stats/weekly/?user=${currentUser}&start=${moment(startDate).format('DD-MM-YYYY')}&end=${moment(endDate).format('DD-MM-YYYY')}`;
-      client.query({query: JOURNAL_QUERY, variables: 
-                      {name: currentUser, 
-                        start_date: moment(startDate).format('DD-MM-YYYY'), 
-                        end_date: moment(endDate).format('DD-MM-YYYY')}})
-                        // check response length and save as required
-                        .then((response) => {(response.data.weeklyStats.results.length > 0) 
-                                              ? setExercises(response.data.weeklyStats)
-                                              : setExercises({success: false})})
-                        // .then((response) => setExercises(response.data))
-                        .catch(error => {
-                          console.error('There was an error fetching the data!', error);
-                        })
-
-      // if (response.data.stats && Array.isArray(response.data.stats)) {
-      //   setExercises(response.data.stats);
-      // } else {
-      //   console.error('Unexpected response structure:', response.data);
-      //   setExercises([]);
-      // }
+      const response = await client.query({
+        query: JOURNAL_QUERY,
+        variables: {
+          name: currentUser,
+          start_date: moment(startDate).format('DD-MM-YYYY'),
+          end_date: moment(endDate).format('DD-MM-YYYY'),
+        },
+      });
+      setExercises(response.data.weeklyStats);
     } catch (error) {
-      console.error('Failed to fetch exercises', error);
+      console.error('Failed to fetch exercises', error.message);
     }
   };
 
@@ -92,6 +64,34 @@ const Journal = ({ currentUser }) => {
     setEndDate(moment(endDate).add(1, 'weeks').endOf('week').toDate());
   };
 
+  const makeExerciseList = () => {
+    if (exercises.hasOwnProperty('results')) {
+      return (exercises.success && exercises.results.length > 0) ? (
+        exercises.results[0].exercises.map((item, index) => (
+          <li key={index} className="exercise-journal-data">
+            {item.exerciseType} - {item.totalDuration} minutes
+          </li>
+        ))
+      ) : (
+        <li>No exercises found for this period.</li>
+      );
+    } else {
+      return <li>No exercises found for this period.</li>;
+    }
+  };
+
+  //   <ul>
+  //   {exercises.success ? (
+  //     exercises.results[0].exercises.map((item, index) => (
+  //       <li key={index} className="exercise-journal-data">
+  //         {item.exerciseType} - {item.totalDuration} minutes
+  //       </li>
+  //     ))
+  //   ) : 
+  //     <li>No exercises found for this period.</li>
+  //   }
+  // </ul>
+
   // TODO: Deal with empty result
   return (
     <div className="journal-container">
@@ -101,18 +101,8 @@ const Journal = ({ currentUser }) => {
         <Button className="button-small" onClick={goToPreviousWeek}>&larr; Previous</Button>
         <span>{moment(startDate).format('DD-MM-YYYY')} to {moment(endDate).format('DD-MM-YYYY')}</span>
         <Button className="button-small" onClick={goToNextWeek}>Next &rarr;</Button>
-        </div>
-      <ul>
-        {exercises.success ? (
-          exercises.results[0].exercises.map((item, index) => (
-            <li key={index} className="exercise-journal-data">
-              {item.exerciseType} - {item.totalDuration} minutes
-            </li>
-          ))
-        ) : (
-          <li>No exercises found for this period.</li>
-        )}
-      </ul>
+      </div> 
+        <ul> {makeExerciseList()} </ul>
     </div>
   );
 };
