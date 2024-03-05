20ce0c1cf5ee1b52198ebe030d4be76a132b8c43
Georgina Baker <georginambaker@outlook.com>
2024-03-05 16:52:50 +0000

    Adding test file for trackExercise & adjustments to setup for running Jest.

--- frontend/src/components/trackExercise.test.js
+++ frontend/src/components/trackExercise.test.js
@@ -1,7 +1,21 @@
+// This is a unit test file for the trackExercise.js component, it is using Jest and is being run by react-scripts.
+// The package.json has to be adapted to be able to run the tests successfully.
+// Docker is used to run the tests, the Frontend Dockerfile needs to contain the 'RUN npm test' command at the end of the build stage.
+// The tests will run AUTOMATICALLY when Docker is run, if the app build successfully then the tests have passed!!
+// The test cases should be updated when new features are added to the trackExercise.js file.
+//
+// --- How to run the tests ---
+// To fix / add to the test file:
+// Change to frontend directory in terminal - 'cd frontend'.
+// Run docker command: 'docker compose run --build --rm frontend test'
+// If they pass, you will not see outcome of tests in the terminal.
+// If they fail, the test results will be shown in the terminal.
+
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import TrackExercise from './trackExercise';
+import { expect } from '@jest/globals';
 
 describe('TrackExercise Component', () => {
   test('renders the component', () => {
@@ -9,33 +23,42 @@ describe('TrackExercise Component', () => {
     expect(container).toBeInTheDocument();
   });
 
-  test('updates state on exercise icon click', () => {
-    const { getByText } = render(<TrackExercise currentUser="testUser" />);
-    fireEvent.click(getByText('Running'));
-    expect(state.exerciseType.toHaveValue('Running'));
+  test('distance is hidden when tracking workout', () => {
+    const { getByTestId } = render(<TrackExercise currentUser="testUser" />);
+    fireEvent.click(getByTestId('Gym Icon'));
+
+    const distance = getByTestId('distance');
+    expect(distance.className).toBe('invisible');
   });
 
-  test('updates state on mood icon click', () => {
-    const { getByText } = render(<TrackExercise currentUser="testUser" />);
-    fireEvent.click(getByText('Happy'));
-    expect(state.mood.toHaveValue('Happy'));
+  test('reps and sets is visible when tracking workout', () => {
+    const { getByTestId } = render(<TrackExercise currentUser="testUser" />);
+    fireEvent.click(getByTestId('Gym Icon'));
+
+    const repsField = getByTestId('reps');
+    const setsField = getByTestId('sets');
+    expect(repsField).toBeVisible();
+    expect(setsField).toBeVisible();
   });
 
   test('speed calculates correctly', () => {
-    const { getByText } = render(<TrackExercise currentUser="testUser" />);
-    fireEvent.input(getByText('10'));
-    expect(state.distance.toHaveValue(10));
-    fireEvent.input(getByText('60'));
-    expect(state.duration.toHaveValue(60));
-    expect(state.speed.toHaveValue(10));
+    const { getByTestId } = render(<TrackExercise currentUser="testUser" />);
+    fireEvent.input(getByTestId('distance').querySelector('input'), { target: { value: '10' } });
+    fireEvent.input(getByTestId('duration').querySelector('input'), { target: { value: '60' } });
+
+    const speedInput = getByTestId('speed').querySelector('input');
+    expect(speedInput.value).toBe('10.00');
   });
 
-  test('submits the form and displays success message', async () => {
-    const { getByText } = render(<TrackExercise currentUser="testUser" />);
-    // Trigger form submission
-    fireEvent.click(getByText('Save activity'));
-    await waitFor(() => {
-      expect(getByText('Activity logged successfully! Well done!')).toBeInTheDocument();
-    });
-  });
+  // To be added when pace is added!!
+  //
+  // test('pace calculates correctly', () => {
+  //   const { getByTestId } = render(<TrackExercise currentUser="testUser" />);
+  //   fireEvent.input(getByTestId('distance').querySelector('input'), { target: { value: '8' } });
+  //   fireEvent.input(getByTestId('duration').querySelector('input'), { target: { value: '45' } });
+  //
+  //   const speedInput = getByTestId('pace').querySelector('input');
+  //   expect(paceInput.value).toBe('5.63');
+  // });
+
 });
