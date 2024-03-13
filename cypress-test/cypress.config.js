4abb5c7473008afb3dee405268adbe6275d1168d
Carlie Ede <carlieede@gmail.com>
2024-03-13 18:01:22 +0000

    sign up

--- cypress-test/cypress.config.js
+++ cypress-test/cypress.config.js
@@ -1,11 +1,34 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+const { connect, disconnect } = require("./cypress/support/db");
 
 const config = {
-  apiUrl: process.env.REACT_APP_API_GATEWAY_URL || 'http://localhost'
+  apiUrl: process.env.REACT_APP_API_GATEWAY_URL || "http://localhost",
 };
 
 module.exports = defineConfig({
-  e2e: {
+  // e2e: {
+  //   setupNodeEvents(on, config) {
+  //     on('task', {
+  //       async clearDB() {
+  // const { activity, auth } = await connect();
+  // const exercises = activity.collection('exercises');
+  // const users = auth.collection('users');
+  // console.log('Cleaning the database');
+  // await activity.dropCollection('exercises');
+  // await auth.dropCollection('user');
+  // await disconnect(); // Disconnect from the database after clearing
+  //         return null;
+  //       }
+  //     });
+  //   }
+  // }
+  env: {
     baseUrl: config.apiUrl,
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
