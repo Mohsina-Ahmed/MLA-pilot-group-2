91fe7592561e0412da0328232cbba5c9e63df7ec
Carlie Ede <carlieede@gmail.com>
2024-03-13 18:01:22 +0000

    sign up

--- cypress-test/cypress/support/db.js
+++ cypress-test/cypress/support/db.js
@@ -1,8 +1,18 @@
-const { MongoClient } = require("mongodb")
+const mongoose = require("mongoose");
 const uri = "mongodb://root:cfgmla23@mongodb:27017"
 
-const client = new MongoClient(uri);
 async function connect() {
-    await client.connect();
-    return client.db('test-database')
+    const activity = mongoose.createConnection(uri, {useNewUrlParser: true, dbName: "activitydb" });
+    const auth = mongoose.createConnection(uri, {useNewUrlParser: true, dbName: "authdb" });
+
+    console.log(mongoose.connection.readyState)
+    return {activity: activity, 
+            auth: auth
+        };
 }
+
+async function disconnect() {
+    await mongoose.disconnect()
+}
+
+module.exports = {connect, disconnect}
