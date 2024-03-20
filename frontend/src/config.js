5f70488d9b9ca07cb6c0ee1b91e3e1d400aa4f89
Mohsina-Ahmed <122023111+Mohsina-Ahmed@users.noreply.github.com>
2024-03-20 23:47:10 +0000

    remove the public ip

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 // api config
 const config = {
-  apiUrl:
-    process.env.REACT_APP_API_GATEWAY_URL ||
-    "http://35.178.173.169/" ||
-    "http://localhost",
+  apiUrl: process.env.REACT_APP_API_GATEWAY_URL || "http://localhost",
 };
 export default config;
 
