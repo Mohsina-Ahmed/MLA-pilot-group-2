67e1a6b18e100eef04e9eea817c01fe498444ed4
Carlie Ede <carlieede@gmail.com>
2024-03-22 19:18:58 +0000

    cypress with .env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 // api config
 const config = {
-    // apiUrl: process.env.REACT_APP_API_GATEWAY_URL || 'http://localhost'
+    apiUrl: process.env.REACT_APP_API_GATEWAY_URL
     // localhost of docker container for cypress tests....
-    apiUrl: "http://172.26.0.1"
+    // apiUrl: "http://172.26.0.1"
 };
 export default config;
+
+console.log(config.apiUrl)
 
 // track exercise
 function getUrl() {
