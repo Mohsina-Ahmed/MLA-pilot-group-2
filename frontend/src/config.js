14fe5c5e83918636b038693d18103b74468a59cd
Mohsina-Ahmed <122023111+Mohsina-Ahmed@users.noreply.github.com>
2024-03-26 00:29:27 +0000

    Fixing config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 // api config
 const config = {
-  apiUrl:
-    "http://mla-fitness-app-v7.mla-fitness-app-v7:8080" ||
-    process.env.REACT_APP_API_GATEWAY_URL ||
-    "http://127.0.0.1:80",
+  apiUrl: process.env.REACT_APP_API_GATEWAY_URL || "http://127.0.0.1:80",
 };
 export default config;
 
