5e0d7d51eb1b0d9e72ea0a969bb29ff8e0e87afa
Carlie Ede <carlieede@gmail.com>
2024-03-31 21:51:00 +0000

    statistics graphql apollo client

--- frontend/src/components/statistics_v2.js
+++ frontend/src/components/statistics_v2.js
@@ -2,66 +2,47 @@ import React, { useState, useEffect } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import './statistics.css';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-import config from '../config';
+import { useQuery, NetworkStatus } from '@apollo/client';
+import { STATS_QUERY } from './queries/graphql';
 
-console.log('on the statistics graphQL page')
 
-// setup the apollo client for graphql 
-const client = new ApolloClient({
-  uri: `${config.apiUrl}/api/graphql`,
-  cache: new InMemoryCache(),
-});
-
-console.log(`${config.apiUrl}/api/graphql`)
-
-// setup the graphql query 
-const STATS_QUERY = gql
-  `query filteredStats($name: String) {
-    filteredStats(name: $name) {
-      success
-      errors
-      results {
-        username 
-        exercises {
-          exerciseType
-          totalDuration
-        }
-      }
-    }
-  }
-  `;
 
 const Statistics = ({currentUser}) => {
   const [data, setData] = useState([]);
 
+  const statsResponse = useQuery(STATS_QUERY, {variables: {name: currentUser}})
+
   // make graphql request
   useEffect(() => {
-    // const url = 'http://localhost:5050/api/graphql';
-    client.query({query: STATS_QUERY, variables: {name: currentUser}})
-      .then(response => {setData(response.data.filteredStats)})
-      .catch(error => {
-        console.error('There was an error fetching the data!', error);
-      });
-  }, [currentUser]);
+    if (statsResponse.data) {
+      const statsResults = statsResponse.data.filteredStats;
+      
+      if (statsResults.success && statsResults.results.length > 0){
+        const statsData = statsResults.results[0].exercises
+        setData(statsData);
+      }
+    }
+
+  }, [statsResponse, currentUser]);
+
+    // handle loading and error states
+    if (statsResponse.loading) return <p>Loading...</p>;
+    if (statsResponse.error) return <p>Error: {statsResponse.error.message}</p>;
+      
 
   // generate exercise list - use of separate function call makes it easier to 
   // check if exercise results exist to avoid access errors
   const makeStatsList = () => {
-    if (data.hasOwnProperty('results')) {
-      return (data.success && data.results.length > 0) ? (
-        data.results[0].exercises.map((item, index) => (
-          <div key={index} className="exercise-data">
+      return (data.length > 0) ? (
+        Object.values(data).map((item, index) => (
+          <li key={index} className="exercise-data">
             <div><strong>{item.exerciseType}</strong></div>
-            <div>Total Duration: {item.totalDuration} min</div>
-          </div>
+            <div>Total Duration: {item.exerciseDuration} min</div>
+          </li>
         ))
       ) : (
         <li>No data available.</li>
       );
-    } else {
-      return <li>No data available.</li>;
-    }
   }
 
   return (
