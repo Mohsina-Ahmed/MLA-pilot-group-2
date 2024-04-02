b0b7854a1f795e0589e2e1272d3213bd236b9842
Georgina Baker <georginambaker@outlook.com>
2024-04-02 12:04:16 +0100

    Adding calories chart to the homepage.

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -54,7 +54,7 @@ const Homepage = ({ currentUser }) => {
       if (caloriesResult.hasOwnProperty('results')) {
         if (caloriesResult.success && caloriesResult.results.length > 0) {
           const calData = caloriesResult.results[0];
-          setCalories({ value: calData.daily_calories });
+          setCalories({ value: parseInt(calData.daily_calories) });
           console.log(calories);
         } else {
           setCalories({ value: 0 });
@@ -75,14 +75,14 @@ const Homepage = ({ currentUser }) => {
 
   return (
     <div className="Homepage-container">
-      <h3 style={{ marginBottom: '40px'}}>🌟 Your Daily Summary 🌟</h3>
+      <h3 style={{ marginBottom: '30px'}}>🌟 Your Daily Summary 🌟</h3>
       <h4 style={{ marginBottom: '20px'}}>Hello, {currentUser}!</h4>
       <p style={{ marginBottom: '10px'}}>How are you today?:</p>
-      <div className="emojis" style={{ marginBottom: '40px'}}>
-        <IconButton className="emoji" color={selectedEmoji === 1 ? "primary" : "default"} onClick={() => updateBackgroundColor(1, '#B9F4F3')}>
+      <div className="emojis" style={{ marginBottom: '25px'}}>
+        <IconButton className="emoji" color={selectedEmoji === 1 ? "primary" : "default"} onClick={() => updateBackgroundColor(1, '#83F0F3')}>
           <Mood fontSize='large' />
         </IconButton>
-        <IconButton className="emoji" color={selectedEmoji === 2 ? "primary" : "default"} onClick={() => updateBackgroundColor(2, '#fffacd')}>
+        <IconButton className="emoji" color={selectedEmoji === 2 ? "primary" : "default"} onClick={() => updateBackgroundColor(2, '#FBF071')}>
           <SentimentSatisfied fontSize='large' />
         </IconButton>
         <IconButton className="emoji" color={selectedEmoji === 3 ? "primary" : "default"} onClick={() => updateBackgroundColor(3, '#A0F0A4')}>
@@ -96,27 +96,64 @@ const Homepage = ({ currentUser }) => {
       <Col>
       {userHasExercise ? (
         <div>
-        <p>Your last exercise was on {lastExercise.date}:</p>
-        <p>Well done on {lastExercise.exercise} for {lastExercise.duration} minutes!</p>
+        <p style={{marginBottom: "5px"}}>Your last exercise was on</p> 
+        <p>{lastExercise.date}:</p>
+        <p style={{marginBottom: "5px"}}>Well done on {lastExercise.exercise}</p>
+        <p>for {lastExercise.duration} minutes!</p>
         <h4>👏👏👏</h4>
         </div>
       ) : (
         <div>
-        <p>You have not logged an activity yet.</p>
-        <Link to="/trackExercise">Head to the Track New Exercise page to try it out!</Link>
+        <p style={{marginBottom: "20px"}}>You have not logged an activity yet.</p>
+        <p style={{marginBottom: "20px"}}>👇 Why don't you try it out? 👇</p>
+        <Link to="/trackExercise">Track New Exercise</Link>
         </div>
       )}
       </Col>
       <Col>
-      <div>
-        <p style={{marginBottom: "20px"}}>Calories Burned Today: {calories.value}</p>
-        <div class="progress" style={{height: "40px", marginBottom: "20px"}}>
-          <div class="progress-bar progress-bar-striped progress-bar-animated" style={{width: "50%"}} color={themeColor} aria-valuenow={parseInt(calories.value)} aria-valuemin="0" aria-valuemax="1000">{calories.value}</div>
-        </div>
+      <div style={{ width: "100%", height: "200px" }}>
+    <p style={{marginBottom: "10px"}}>Calories Burned Today:</p>
+    <div style={{ width: "100%", height: "110px" }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadialBarChart 
+          startAngle={90}
+          endAngle={-270} 
+          innerRadius={45} 
+          outerRadius={60} 
+          barSize={15} 
+          data={[{ value: calories.value }]}
+        >
+          {/* Title */}
+          <PolarAngleAxis
+            type="number"
+            domain={[0, 1000]}
+            angleAxisId={0}
+            tick={false}
+          />
+          <RadialBar
+            minAngle={5}
+            background
+            clockWise={true}
+            cornerRadius={10 / 2}
+            dataKey="value"
+          >
+            <Cell
+              key={`cell-1`}
+              fill={calories.value === 1000 ? "#8884d8" : themeColor} // adjust colour for 100% goal
+            />
+          </RadialBar>
+          {/* Label component for text */}
+          <text x='50%' y='50%' textAnchor='middle' style={{ fontSize: 20, fontWeight: 'bold', dominantBaseline:'middle' }}>
+            {`${calories.value}`}
+          </text>
+          <Tooltip />
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </div>
         { calories.value > 0 ? (
-          <p>Great work today 🔥</p>
+          <p style={{marginBottom: "0px"}}>Great work today 🔥</p>
         ) : (
-          <p>Have you tracked today's exercise?</p>
+          <p style={{marginBottom: "0px"}}>No exercise tracked today!</p>
         )
         }
       </div>
